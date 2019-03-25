b54ec229965bdff7a5a8e2da8421b39ed4c00871
pamunoz <pfariasmunoz@gmail.com>
2019-03-25 17:22:40 -0300

    Change the on Create to be for dialog

--- PickerForDat/app/src/main/java/com/pfariasmunoz/pickerfordat/DatePickerFragment.kt
+++ PickerForDat/app/src/main/java/com/pfariasmunoz/pickerfordat/DatePickerFragment.kt
@@ -2,14 +2,11 @@ package com.pfariasmunoz.pickerfordat
 
 
 import android.app.DatePickerDialog
+import android.app.Dialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.DatePicker
-import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass.
@@ -17,16 +14,11 @@ import androidx.fragment.app.DialogFragment
  */
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
     }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+    override fun onDateSet(datePicker: DatePicker?, year: Int, month: Int, day: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
