958ebe016aeb68498d91e9d58d284e99f1adf9df
pamunoz <pfariasmunoz@gmail.com>
2019-03-25 17:19:21 -0300

    Change the extension and implementations of the Fragment

--- PickerForDat/app/src/main/java/com/pfariasmunoz/pickerfordat/DatePickerFragment.kt
+++ PickerForDat/app/src/main/java/com/pfariasmunoz/pickerfordat/DatePickerFragment.kt
@@ -1,18 +1,21 @@
 package com.pfariasmunoz.pickerfordat
 
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class DatePickerFragment : Fragment() {
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,10 @@ class DatePickerFragment : Fragment() {
         return TextView(activity).apply {
             setText(R.string.hello_blank_fragment)
         }
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 
