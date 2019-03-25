4b6c35878116616cb28368825647b697c64b79a1
pamunoz <pfariasmunoz@gmail.com>
2019-03-25 16:52:22 -0300

    Add the body to respond to the dialogs

--- DialogForAlert/app/src/main/java/com/pfariasmunoz/dialogforalert/MainActivity.kt
+++ DialogForAlert/app/src/main/java/com/pfariasmunoz/dialogforalert/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.pfariasmunoz.dialogforalert
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickShowAlert(view: View) {
-        
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this).apply {
+            title = "Alert"
+            setMessage("Click OK to continue, or Cancel to stop:")
+            setPositiveButton("Ok") { dialog, which ->  
+
+            }
+        }
+
     }
 }
