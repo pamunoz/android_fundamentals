4524bc15e1d3e370dcb5a59ea7802a95cbcc36cd
pamunoz <pfariasmunoz@gmail.com>
2019-03-25 16:43:45 -0300

    Add the button and the textview to show the alert

--- DialogForAlert/app/src/main/java/com/pfariasmunoz/dialogforalert/MainActivity.kt
+++ DialogForAlert/app/src/main/java/com/pfariasmunoz/dialogforalert/MainActivity.kt
@@ -2,11 +2,16 @@ package com.pfariasmunoz.dialogforalert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickShowAlert(view: View) {
+        
     }
 }
