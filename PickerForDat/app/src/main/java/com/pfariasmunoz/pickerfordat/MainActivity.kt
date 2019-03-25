329f64bd473aa5855e323e5198e73ee05648e384
pamunoz <pfariasmunoz@gmail.com>
2019-03-25 17:14:32 -0300

    Add the layout to deal with the datepicker

--- PickerForDat/app/src/main/java/com/pfariasmunoz/pickerfordat/MainActivity.kt
+++ PickerForDat/app/src/main/java/com/pfariasmunoz/pickerfordat/MainActivity.kt
@@ -2,11 +2,16 @@ package com.pfariasmunoz.pickerfordat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showDatePicker(view: View) {
+        
     }
 }
