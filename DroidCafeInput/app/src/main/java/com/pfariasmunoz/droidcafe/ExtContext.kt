704a219ef83c91886462a819e344c349b76857da
pamunoz <pfariasmunoz@gmail.com>
2019-03-25 15:17:25 -0300

    Add annotation to check if resource

--- DroidCafeInput/app/src/main/java/com/pfariasmunoz/droidcafe/ExtContext.kt
+++ DroidCafeInput/app/src/main/java/com/pfariasmunoz/droidcafe/ExtContext.kt
@@ -2,10 +2,11 @@ package com.pfariasmunoz.droidcafe
 
 import android.content.Context
 import android.widget.Toast
+import androidx.annotation.IntegerRes
 
 fun Context.toast(res: Any, duration: Int = Toast.LENGTH_SHORT) {
     when (res) {
-        is Int -> Toast.makeText(this, resources.getString(res).toString(), duration).show()
+        is @IntegerRes Int -> Toast.makeText(this, resources.getString(res).toString(), duration).show()
         is String -> Toast.makeText(this, res, duration).show()
         else -> return
     }
