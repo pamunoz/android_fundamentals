06c2b3fd6d8e76819067a7e94c0a2e8f153050f3
pamunoz <pfariasmunoz@gmail.com>
2019-03-26 15:26:58 -0300

    Set the pager and its adapter

--- TabExperiment/app/src/main/java/com/pfariasmunoz/tabexperiment/MainActivity.kt
+++ TabExperiment/app/src/main/java/com/pfariasmunoz/tabexperiment/MainActivity.kt
@@ -2,7 +2,6 @@ package com.pfariasmunoz.tabexperiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,15 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar as Toolbar)
-        (tab_layout as TabLayout).apply {
+        setSupportActionBar(toolbar)
+        tab_layout.apply {
             // Set the text for each tab.
-            addTab(((tab_layout as TabLayout).newTab().setText(R.string.tab_label1)))
-            addTab(((tab_layout as TabLayout).newTab().setText(R.string.tab_label2)))
-            addTab(((tab_layout as TabLayout).newTab().setText(R.string.tab_label3)))
+            addTab((tab_layout.newTab().setText(R.string.tab_label1)))
+            addTab((tab_layout.newTab().setText(R.string.tab_label2)))
+            addTab((tab_layout.newTab().setText(R.string.tab_label3)))
             // Set the tabs to fill the entire layout.
-            (tab_layout as TabLayout).tabGravity = TabLayout.GRAVITY_FILL
+            tab_layout.tabGravity = TabLayout.GRAVITY_FILL
             // Use PagerAdapter to manage page views in fragments.
+            val adapter = PagerAdapter(supportFragmentManager, tab_layout.tabCount)
+            pager.adapter = adapter
         }
     }
 }
