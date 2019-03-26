6829ba3f993fbd9bc640fd3d8eda68f08b3f0dcd
pamunoz <pfariasmunoz@gmail.com>
2019-03-26 14:48:09 -0300

    Set the supoortActionBar

--- TabExperiment/app/src/main/java/com/pfariasmunoz/tabexperiment/MainActivity.kt
+++ TabExperiment/app/src/main/java/com/pfariasmunoz/tabexperiment/MainActivity.kt
@@ -2,11 +2,14 @@ package com.pfariasmunoz.tabexperiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar as Toolbar)
     }
 }
