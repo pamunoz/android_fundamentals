2506d38d6861a9176705e6c22d713e6536bba765
pamunoz <pfariasmunoz@gmail.com>
2019-03-26 15:05:30 -0300

    set the support action bar

--- TabExperiment/app/src/main/java/com/pfariasmunoz/tabexperiment/MainActivity.kt
+++ TabExperiment/app/src/main/java/com/pfariasmunoz/tabexperiment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pfariasmunoz.tabexperiment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar as Toolbar)
+        (tab_layout as TabLayout).apply {
+            // Set the text for each tab.
+            addTab(((tab_layout as TabLayout).newTab().setText(R.string.tab_label1)))
+            addTab(((tab_layout as TabLayout).newTab().setText(R.string.tab_label2)))
+            addTab(((tab_layout as TabLayout).newTab().setText(R.string.tab_label3)))
+            // Set the tabs to fill the entire layout.
+            (tab_layout as TabLayout).tabGravity = TabLayout.GRAVITY_FILL
+            // Use PagerAdapter to manage page views in fragments.
+        }
     }
 }
