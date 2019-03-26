df70845d57b4fa8ab3d173abe90922ccc90d2aa2
pamunoz <pfariasmunoz@gmail.com>
2019-03-26 18:00:18 -0300

    Add the adapter!

--- MyRecyclerview/app/src/main/java/com/pfariasmunoz/myrecyclerview/WordListAdapter.kt
+++ MyRecyclerview/app/src/main/java/com/pfariasmunoz/myrecyclerview/WordListAdapter.kt
@@ -1,22 +1,28 @@
 package com.pfariasmunoz.myrecyclerview
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
-class WordListAdapter: RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+class WordListAdapter(ctx: Context, private val wordList: LinkedList<String>): RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(ctx)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-
+        val itemView = inflater.inflate(R.layout.wordlist_item, parent, false)
+        return WordViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int {
-    }
+    override fun getItemCount() = wordList.size
 
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        (holder.itemView as TextView).text = wordList[position]
     }
 
-    class WordViewHolder(itemView: View, val adapter: WordListAdapter): RecyclerView.ViewHolder(itemView) {
-
-    }
+    class WordViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
 }
