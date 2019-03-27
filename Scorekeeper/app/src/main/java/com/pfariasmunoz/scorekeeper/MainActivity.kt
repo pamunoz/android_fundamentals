dcd056389d075908b86d53fd4669d34ef00e6b14
pamunoz <pfariasmunoz@gmail.com>
2019-03-27 09:26:25 -0300

    Create the scorekeeper project

---
+++ Scorekeeper/app/src/main/java/com/pfariasmunoz/scorekeeper/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pfariasmunoz.scorekeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
