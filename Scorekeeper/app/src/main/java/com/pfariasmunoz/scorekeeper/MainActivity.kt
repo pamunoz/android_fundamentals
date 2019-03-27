27e98b128c727ef85073b4c217333fc7795e1952
pamunoz <pfariasmunoz@gmail.com>
2019-03-27 16:55:00 -0300

    Add a night mode switcher to the app

--- Scorekeeper/app/src/main/java/com/pfariasmunoz/scorekeeper/MainActivity.kt
+++ Scorekeeper/app/src/main/java/com/pfariasmunoz/scorekeeper/MainActivity.kt
@@ -3,8 +3,10 @@ package com.pfariasmunoz.scorekeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.annotation.IntegerRes
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -48,5 +50,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        //Check if the correct item was clicked
+        if(item?.itemId == R.id.night_mode) {
+            // Get the night mode state of the app
+            val nightMode = AppCompatDelegate.getDefaultNightMode()
+            //Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+            }
+            // Recreate the activity for the theme change to take effect.
+            recreate()
+        }
+        return true
     }
 }
