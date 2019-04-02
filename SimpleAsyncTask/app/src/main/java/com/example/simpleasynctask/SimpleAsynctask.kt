838ee81d2d9ac423e2236cf32d126c3053f55816
pamunoz <pfariasmunoz@gmail.com>
2019-04-02 12:12:00 -0300

    Add a weakreference to a textview that needs to be updated on the aysnctask

--- SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsynctask.kt
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsynctask.kt
@@ -1,10 +1,16 @@
 package com.example.simpleasynctask
 
 import android.os.AsyncTask
+import android.widget.TextView
+import java.lang.ref.WeakReference
 
-class SimpleAsynctask : AsyncTask<Any, Any, String>() {
+class SimpleAsynctask(textView: TextView) : AsyncTask<Any, Any, String>() {
 
-    
+    /** The AsyncTask needs to update the TextView in the Activity once it has
+     * completed sleeping (in the onPostExecute() method). The constructor for
+     * the class will therefore need a reference to the TextView to be updated.  */
+    private val mTextView: WeakReference<TextView> = WeakReference(textView)
+
     override fun doInBackground(vararg params: Any?): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
