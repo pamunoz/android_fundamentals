37a7ef87f212f3d504f836dea3eca6a2b14bc648
pamunoz <pfariasmunoz@gmail.com>
2019-04-02 12:29:28 -0300

    Implement the Asynctask subclass

--- SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.kt
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startTask(view: View) {
-
+        // Put a message in the text view
+        tv_task.text = getString(R.string.sleep_message)
+        // Start the AsyncTask.
+        SimpleAsynctask(tv_task).execute()
     }
 }
