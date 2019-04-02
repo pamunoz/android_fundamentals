dd1ef058789acb1ae31336d9bbefc97ae97f0bf7
pamunoz <pfariasmunoz@gmail.com>
2019-04-02 12:06:20 -0300

    Add the file class for the asynctaks

--- SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.kt
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.kt
@@ -13,6 +13,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startTask(view: View) {
-        
+
     }
 }
---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsynctask.kt
@@ -0,0 +1,12 @@
+package com.example.simpleasynctask
+
+import android.os.AsyncTask
+
+class SimpleAsynctask : AsyncTask<Any, Any, String>() {
+
+    
+    override fun doInBackground(vararg params: Any?): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
