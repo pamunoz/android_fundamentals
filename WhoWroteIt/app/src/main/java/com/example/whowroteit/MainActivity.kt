cdacab127654db5252c5c1d915f381420eccf057
pamunoz <pfariasmunoz@gmail.com>
2019-04-02 15:03:30 -0300

    Configure kotlin on the project

---
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.whowroteit
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun searchBooks(view: View) {
+        // Get the search string from the input field.
+
+    }
+}
