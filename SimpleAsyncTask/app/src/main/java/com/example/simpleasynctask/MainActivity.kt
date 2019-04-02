e3430f722887d1c0a5113ee305a171d030d3ae31
pamunoz <pfariasmunoz@gmail.com>
2019-04-02 11:47:19 -0300

    Change to kotlin and add method to button start task

---
+++ SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.simpleasynctask
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startTask(view: View) {
+        
+    }
+}
