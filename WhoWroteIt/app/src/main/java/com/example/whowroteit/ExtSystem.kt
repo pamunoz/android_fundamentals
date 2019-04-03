d729e65cbfc958d1a7411611bef6f8a8b435de3a
pamunoz <pfariasmunoz@gmail.com>
2019-04-03 13:51:28 -0300

    Add a check on the internet connection

--- WhoWroteIt/app/src/main/java/com/example/whowroteit/ExtSystem.kt
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/ExtSystem.kt
@@ -1,10 +1,21 @@
 package com.example.whowroteit
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun View.hideTheKeyBoard(context: Context) {
     val inputManager: InputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputManager.hideSoftInputFromWindow(this.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+}
+
+fun Context.isNetworkConnected(context: Context): Boolean {
+    val manager: ConnectivityManager? = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    var networkInfo: NetworkInfo? = null
+    if (manager != null) {
+        networkInfo = manager.activeNetworkInfo
+    }
+    return (networkInfo != null && networkInfo.isConnected)
 }
