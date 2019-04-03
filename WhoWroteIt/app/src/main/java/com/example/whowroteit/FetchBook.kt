8987b99a6bab662d2c4684b103e3a8dbeaf24318
pamunoz <pfariasmunoz@gmail.com>
2019-04-03 08:38:27 -0300

    Simplify code to get json

--- WhoWroteIt/app/src/main/java/com/example/whowroteit/FetchBook.kt
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/FetchBook.kt
@@ -1,6 +1,7 @@
 package com.example.whowroteit
 
 import android.os.AsyncTask
+import android.util.Log
 import android.widget.TextView
 import org.json.JSONArray
 import org.json.JSONException
@@ -17,14 +18,19 @@ class FetchBook(titleText: TextView, authorText: TextView): AsyncTask<String, Un
 
     override fun onPostExecute(result: String?) {
         super.onPostExecute(result)
+        Log.d("pablo", result)
         try {
-            val itemsArray: JSONArray = JSONObject(result).getJSONArray("items")
+            // Convert the response into a JSON object.
+            val jsonObject: JSONObject = JSONObject(result)
+            // Get the JSONArray of book items.
+            val itemsArray: JSONArray = jsonObject.getJSONArray("items")
             var i = 0
             var title: String? = null
             var authors: String? = null
             while (i < itemsArray.length() && (authors == null && title == null)) {
                 // Get the current item information
                 val book: JSONObject = itemsArray.getJSONObject(i)
+
                 val volumeInfo: JSONObject = book.getJSONObject("volumeInfo")
 
                 // try to get the author and title from the current item
@@ -53,7 +59,7 @@ class FetchBook(titleText: TextView, authorText: TextView): AsyncTask<String, Un
         } catch (e: JSONException) {
             // If postExecute does not receive a proper JSONString,
             // update the UI to show failed results
-            mTitleText.get()?.text = mTitleText.get()?.resources.getString(R.string.no_result)
+            mTitleText.get()?.text = mTitleText.get()?.resources?.getString(R.string.no_result)
             mAuthorText.get()?.text = ""
             e.printStackTrace()
         }
--- WhoWroteIt/app/src/main/java/com/example/whowroteit/NetworkUtils.kt
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/NetworkUtils.kt
@@ -23,60 +23,11 @@ object NetworkUtils {
 
 
     fun getBookInfo(query: String): String? {
-        var urlConnection: HttpURLConnection? = null
-        var bookJSONString: String? = null
-
-        try {
-            // Build the full query URI, limiting results to 10 items and
-            // printed books.
-            val builtUri = Uri.parse(BOOK_BASE_URL).buildUpon()
-                    .appendQueryParameter(QUERY_PARAM, query)
-                    .appendQueryParameter(MAX_RESULTS, "10")
-                    .appendQueryParameter(PRINT_TYPE, "books")
-                    .build()
-
-            // Convert the URI to a URL,
-            val requestURL = URL(builtUri.toString())
-
-            // Open the network connection.
-            urlConnection = requestURL.openConnection() as HttpURLConnection
-            urlConnection.apply {
-                requestMethod = "GET"
-            }.connect()
-
-            // Get the InputStream.
-            val inputStream = urlConnection.inputStream
-            //if (builder.length == 0) ""
-            bookJSONString = readInputStream(inputStream)
-
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            // Close the connection and the buffered reader.
-            urlConnection?.disconnect()
-
-        }
-        Log.d(LOG, bookJSONString)
-        return bookJSONString
-    }
-
-    private fun readInputStream(inputStream: InputStream): String? {
-        // Create a buffered reader from that input stream.
-        val reader = BufferedReader(InputStreamReader(inputStream))
-
-        // Use a StringBuilder to hold the incoming response.
-        val builder = StringBuilder()
-
-        // Read the input line-by-line into the string while there is still input:
-        while (reader.readLine() != null) {
-            // Add the current line to the string.
-            builder.append(reader.readLine())
-
-            // Since it's JSON, adding a newline isn't necessary (it won't
-            // affect parsing) but it does make debugging a *lot* easier
-            // if you print out the completed buffer for debugging.
-            builder.append("\n")
-        }
-        return if (builder.isEmpty()) return null else builder.toString()
+        val builtUri = Uri.parse(BOOK_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, query)
+                .appendQueryParameter(MAX_RESULTS, "10")
+                .appendQueryParameter(PRINT_TYPE, "books")
+                .build()
+        return URL(builtUri.toString()).readText()
     }
 }
