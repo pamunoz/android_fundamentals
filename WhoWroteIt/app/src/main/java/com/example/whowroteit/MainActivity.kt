429ab48d2de0861df06ce586802dbc7644753db5
pamunoz <pfariasmunoz@gmail.com>
2019-04-03 08:44:26 -0300

    Hide the keyboard after clicking the button

--- WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.kt
+++ WhoWroteIt/app/src/main/java/com/example/whowroteit/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.whowroteit
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     fun searchBooks(view: View) {
         // Get the search string from the input field.
         val queryString = et_bookInput.text.toString()
+        val inputManager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
         FetchBook(tv_titleText, tv_authorText).execute(queryString)
     }
 }
