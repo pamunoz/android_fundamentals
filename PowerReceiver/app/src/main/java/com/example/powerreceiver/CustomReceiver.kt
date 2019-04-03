6ee047ed7ed27d091557c5d637f575086dab396f
pamunoz <pfariasmunoz@gmail.com>
2019-04-03 15:38:07 -0300

    Add a broadcast receiveer stub class

---
+++ PowerReceiver/app/src/main/java/com/example/powerreceiver/CustomReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.powerreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CustomReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("CustomReceiver.onReceive() is not implemented")
+    }
+}
