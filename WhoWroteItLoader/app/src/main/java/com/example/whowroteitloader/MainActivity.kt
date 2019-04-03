bded8e5f42695c38069a0e55fb93f7cfa3c90289
pamunoz <pfariasmunoz@gmail.com>
2019-04-03 14:45:39 -0300

    Modify the main activity to implement the loadercallbacks

--- WhoWroteItLoader/app/src/main/java/com/example/whowroteitloader/MainActivity.kt
+++ WhoWroteItLoader/app/src/main/java/com/example/whowroteitloader/MainActivity.kt
@@ -3,21 +3,22 @@ package com.example.whowroteitloader
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : AppCompatActivity() , LoaderManager.LoaderCallbacks<String>{
 
     fun searchBooks(view: View) {
         // Get the search string from the input field.
         val queryString = et_bookInput.text.toString()
         view.hideTheKeyBoard(this)
         if (isNetworkConnected && queryString.isNotEmpty()) {
-            FetchBook(tv_titleText, tv_authorText).execute(queryString)
+            val queryBundle = Bundle().apply {
+                putString("queryString", queryString)
+            }
+            supportLoaderManager.restartLoader(0, queryBundle, this)
+
             tv_authorText.text = ""
             tv_titleText.text = resources.getString(R.string.loading)
         } else {
@@ -29,6 +30,23 @@ class MainActivity : AppCompatActivity() {
                 tv_titleText.text = resources.getString(R.string.no_network)
             }
         }
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<String> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLoadFinished(loader: Loader<String>, data: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLoaderReset(loader: Loader<String>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
 }
