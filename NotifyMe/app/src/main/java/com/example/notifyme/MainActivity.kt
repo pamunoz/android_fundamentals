3ccade27c6ec2f32eac7cf8e30c08cd17c54a9d8
pamunoz <pfariasmunoz@gmail.com>
2019-04-04 10:15:51 -0300

    Create the notification channel with the notification manager

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.kt
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.notifyme
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mNotificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +23,24 @@ class MainActivity : AppCompatActivity() {
 
     fun sendNotification() {
 
+    }
+
+    companion object {
+        private const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+    }
+
+    fun createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // Create a Notification Channel
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID, "Mascot Notification",
+                NotificationManager.IMPORTANCE_HIGH).apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = "Notification from Mascot"
+            }
+            mNotificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 }
