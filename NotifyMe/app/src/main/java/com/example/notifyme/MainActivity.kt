29ed6b81d4962d39ad6ead22725ff11b90db8ee2
pamunoz <pfariasmunoz@gmail.com>
2019-04-04 09:45:33 -0300

    Add a stub method on the listener of the button

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.kt
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.notifyme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_notify.setOnClickListener {
+            sendNotification()
+        }
+    }
+
+    fun sendNotification() {
+
     }
 }
