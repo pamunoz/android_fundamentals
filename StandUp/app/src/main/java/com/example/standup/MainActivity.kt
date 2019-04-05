52cff660dfc9c3f103c187cb84fd143c4b062883
pamunoz <pfariasmunoz@gmail.com>
2019-04-05 14:33:53 -0300

    Change the setate of the button is the alarm is already set on

--- StandUp/app/src/main/java/com/example/standup/MainActivity.kt
+++ StandUp/app/src/main/java/com/example/standup/MainActivity.kt
@@ -7,10 +7,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
-import androidx.core.app.NotificationCompat
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,22 +21,37 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+
         val notifyIntent = Intent(this, AlarmReceiver::class.java)
-        val notifyPendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        /**
+         * To track the state of the alarm, you need a boolean variable that is true if the alarm exists,
+         * and false otherwise. To set this boolean, you can call PendingIntent.getBroadcast() with the
+         * FLAG_NO_CREATE flag. If a PendingIntent exists, that PendingIntent is returned;
+         * otherwise the call returns null.
+         */
+        val alarmUp =
+            (PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_NO_CREATE) != null)
+        btn_alarm_toggle.isEnabled = alarmUp
+        val notifyPendingIntent =
+            PendingIntent.getBroadcast(this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT)
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         btn_alarm_toggle.setOnCheckedChangeListener { _, isChecked ->
             val repeatInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES
             val triggerTime = SystemClock.elapsedRealtime() + repeatInterval
             if (isChecked) {
-                alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, repeatInterval, notifyPendingIntent)
+                alarmManager.setInexactRepeating(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    triggerTime,
+                    repeatInterval,
+                    notifyPendingIntent
+                )
                 toast(R.string.alarm_on_message)
             } else {
                 //Cancel notification if the alarm is turned off
@@ -61,7 +75,8 @@ class MainActivity : AppCompatActivity() {
             // Create the NotificationChannel with all the parameters.
             val notificationChannel = NotificationChannel(
                 PRIMARY_CHANNEL_ID, "Stand Up Notification",
-                NotificationManager.IMPORTANCE_HIGH).apply {
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
                 enableLights(true)
                 lightColor = Color.RED
                 enableVibration(true)
