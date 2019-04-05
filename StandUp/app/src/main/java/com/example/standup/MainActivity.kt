bbe1e123e492ea5fb09f08a7658830cdc36625e7
pamunoz <pfariasmunoz@gmail.com>
2019-04-05 13:45:11 -0300

    Fix calling the create the notification channel

--- StandUp/app/src/main/java/com/example/standup/MainActivity.kt
+++ StandUp/app/src/main/java/com/example/standup/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +27,16 @@ class MainActivity : AppCompatActivity() {
         mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         btn_alarm_toggle.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) toast(R.string.alarm_on_message) else toast(R.string.alarm_off_message)
+            if (isChecked) {
+                deliverNotification(this@MainActivity)
+                toast(R.string.alarm_on_message)
+            } else {
+                //Cancel notification if the alarm is turned off
+                mNotificationManager?.cancelAll()
+                toast(R.string.alarm_off_message)
+            }
         }
+        createNotificationChannel()
     }
 
     /**
@@ -55,5 +64,14 @@ class MainActivity : AppCompatActivity() {
     private fun deliverNotification(context: Context) {
         val contentIntent = Intent(context, MainActivity::class.java)
         val contentPendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val builder = NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_stand_up)
+            .setContentTitle("Stand Up Alert")
+            .setContentText("You should stand up and walk around now!")
+            .setContentIntent(contentPendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+        mNotificationManager?.notify(NOTIFICATION_ID, builder.build())
     }
 }
