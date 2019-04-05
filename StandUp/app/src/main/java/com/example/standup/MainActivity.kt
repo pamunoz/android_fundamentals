e101aa2c4bf123386f679b5bf18c75d5746455a4
pamunoz <pfariasmunoz@gmail.com>
2019-04-05 12:44:54 -0300

    Create a notification manager

--- StandUp/app/src/main/java/com/example/standup/MainActivity.kt
+++ StandUp/app/src/main/java/com/example/standup/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.standup
 
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var mNotificationManager: NotificationManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         btn_alarm_toggle.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) toast(R.string.alarm_on_message) else toast(R.string.alarm_off_message)
