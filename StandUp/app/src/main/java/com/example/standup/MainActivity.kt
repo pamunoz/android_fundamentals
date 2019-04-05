92929954930c70a5713136a0d51556c60eea4784
pamunoz <pfariasmunoz@gmail.com>
2019-04-05 12:41:52 -0300

    React to togglebutton

--- StandUp/app/src/main/java/com/example/standup/MainActivity.kt
+++ StandUp/app/src/main/java/com/example/standup/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.standup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_alarm_toggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) toast(R.string.alarm_on_message) else toast(R.string.alarm_off_message)
+        }
     }
 }
