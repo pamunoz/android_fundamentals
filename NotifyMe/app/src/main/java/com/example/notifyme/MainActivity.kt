5f38cf9d9286c6330de3103fe10b214664236740
pamunoz <pfariasmunoz@gmail.com>
2019-04-05 09:25:27 -0300

    Convert the image to a bitmap

--- NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.kt
+++ NotifyMe/app/src/main/java/com/example/notifyme/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -29,7 +30,9 @@ class MainActivity : AppCompatActivity() {
         val notifyBuilder = getNotificationBuilder()
         mNotificationManager?.notify(NOTIFICATION_ID, notifyBuilder.build())
     }
-    private fun updateNotification() {}
+    private fun updateNotification() {
+        val androidImage = BitmapFactory.decodeResource(resources, R.drawable.mascot_1)
+    }
     private fun cancelNotification() {
         mNotificationManager?.cancel(NOTIFICATION_ID)
     }
