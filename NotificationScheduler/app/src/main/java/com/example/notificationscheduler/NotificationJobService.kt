e4152159e5221ba09abb88d32ba3b97eb4dec696
pamunoz <pfariasmunoz@gmail.com>
2019-04-08 07:42:16 -0400

    Create the function to create the notification channel

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
@@ -1,12 +1,13 @@
 package com.example.notificationscheduler
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
+import android.graphics.Color
 
 class NotificationJobService : JobService() {
-    
+
     companion object {
         // Notification channel ID.
         val PRIMARY_CHANNEL_ID ="primary_notification_channel"
@@ -18,6 +19,22 @@ class NotificationJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun createNotificationChannel() {
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // Create the NotificationChannel with all the parameters.
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID, "Job service notification", NotificationManager.IMPORTANCE_HIGH).apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = "Notification from Job service"
+            }
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 
 }
