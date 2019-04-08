210fc2a822ef34e2dc0ffac0e8a6cafc5efe33c1
pamunoz <pfariasmunoz@gmail.com>
2019-04-08 08:20:45 -0400

    Construct and deliver the notification

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
@@ -2,23 +2,43 @@ package com.example.notificationscheduler
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
 import android.graphics.Color
+import androidx.core.app.NotificationCompat
 
 class NotificationJobService : JobService() {
 
     companion object {
         // Notification channel ID.
-        val PRIMARY_CHANNEL_ID ="primary_notification_channel"
+        const val PRIMARY_CHANNEL_ID ="primary_notification_channel"
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Creates the notification channel
+        createNotificationChannel()
+
+        // Set up the notification content intent to launch the app when clicked
+        val contentPendingIntent = PendingIntent.getActivity(this, 0,
+            Intent(this, MainActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID).apply {
+            setContentTitle("Job Service")
+            setContentText("Your job ran to completion!")
+            setContentIntent(contentPendingIntent)
+            setSmallIcon(R.drawable.ic_job_running)
+            priority = NotificationCompat.PRIORITY_HIGH
+            setDefaults(NotificationCompat.DEFAULT_ALL)
+            setAutoCancel(true)
+        }
+        notificationManager.notify(0, builder.build())
+        return false
     }
 
     fun createNotificationChannel() {
