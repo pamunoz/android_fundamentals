91dc560ae80f01e158eb9556fc677d09ece86e13
pamunoz <pfariasmunoz@gmail.com>
2019-04-08 06:59:58 -0400

    Add the notification manager member and the id for the channel

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
@@ -1,9 +1,16 @@
 package com.example.notificationscheduler
 
+import android.app.NotificationManager
 import android.app.job.JobParameters
 import android.app.job.JobService
 
 class NotificationJobService : JobService() {
+
+    private val mNotificationManager: NotificationManager? = null
+
+    companion object {
+        val PRIMARY_CHANNEL_ID ="primary_notification_channel"
+    }
 
     override fun onStopJob(params: JobParameters?): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
