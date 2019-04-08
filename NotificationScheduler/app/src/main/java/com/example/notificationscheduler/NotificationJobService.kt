6153c7be7f754ece862202883158ca08d49cc452
pamunoz <pfariasmunoz@gmail.com>
2019-04-08 06:57:49 -0400

    Add icon for the notification

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.kt
@@ -4,6 +4,7 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 
 class NotificationJobService : JobService() {
+
     override fun onStopJob(params: JobParameters?): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
