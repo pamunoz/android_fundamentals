b837d9bed57e5a29e98b12c464d3cb580280eb7e
pamunoz <pfariasmunoz@gmail.com>
2019-04-08 09:57:58 -0400

    Get the selected options and create the job scheduler and jobinfo objects

--- NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.kt
+++ NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.example.notificationscheduler
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mJobScheduler: JobScheduler? = null
+    companion object {
+        const val JOB_ID = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun scheduleJob(view: View) {
+
+        // Get the selected network option
+        val selectedNetworkId = rg_network_options.checkedRadioButtonId
+        var selectedNetworkOption = when(selectedNetworkId) {
+            R.id.rb_no_network -> JobInfo.NETWORK_TYPE_NONE
+            R.id.rb_any_network -> JobInfo.NETWORK_TYPE_ANY
+            R.id.rb_wifi_network -> JobInfo.NETWORK_TYPE_UNMETERED
+            else -> JobInfo.NETWORK_TYPE_NONE
+        }
+
+        // Create the job scheduler and the job info object
+        mJobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val serviceName = ComponentName(packageName, NotificationJobService::class.java.name)
+        val builder = JobInfo.Builder(JOB_ID, serviceName)
+        builder.setRequiredNetworkType(selectedNetworkOption)
+        val jobInfo: JobInfo = builder.build()
+        mJobScheduler?.schedule(jobInfo)
+        Toast.makeText(this, "Job Scheduled, job will run when " +
+                "the constraints are met.", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    fun cancelJobs(view: View) {
+
     }
 }
