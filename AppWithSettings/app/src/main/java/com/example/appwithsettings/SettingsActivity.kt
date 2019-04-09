dc94cad8dfe32ccff8936ecb3f5e8a72121450ad
pamunoz <pfariasmunoz@gmail.com>
2019-04-09 08:21:01 -0400

    Add aconstant for value of a setting

--- AppWithSettings/app/src/main/java/com/example/appwithsettings/SettingsActivity.kt
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/SettingsActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class SettingsActivity : AppCompatActivity() {
 
+    companion object {
+        const val KEY_PREF_EXAMPLE_SWITCH = "example_switch"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
