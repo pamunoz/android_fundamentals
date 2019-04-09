e5a18c63784b5eb8fa4929d8f77640a494cbcad2
pamunoz <pfariasmunoz@gmail.com>
2019-04-09 07:50:25 -0400

    Connect the main activity with setting activity

--- AppWithSettings/app/src/main/java/com/example/appwithsettings/MainActivity.kt
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appwithsettings
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,7 +34,11 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val settingsIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(settingsIntent)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
