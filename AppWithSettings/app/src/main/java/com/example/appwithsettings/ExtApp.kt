3d5625b6662fdeb56417caa23f49e0b7ba73217e
pamunoz <pfariasmunoz@gmail.com>
2019-04-09 08:23:19 -0400

    Add extension gor getting the default shared preferences

--- AppWithSettings/app/src/main/java/com/example/appwithsettings/ExtApp.kt
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/ExtApp.kt
@@ -1,10 +1,16 @@
 package com.example.appwithsettings
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 fun AppCompatActivity.displayPreferenceFragmentCompat(fragment: PreferenceFragmentCompat) {
     this.supportFragmentManager.beginTransaction()
         .replace(android.R.id.content, fragment)
         .commit()
 }
+
+val Context.defualtSharedPreferences: SharedPreferences
+    get() = PreferenceManager.getDefaultSharedPreferences(this)
