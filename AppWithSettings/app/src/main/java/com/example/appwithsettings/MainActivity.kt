89254e23364942f59a8f2aabdf64480c7aba811f
pamunoz <pfariasmunoz@gmail.com>
2019-04-09 07:47:55 -0400

    Move the code to the correct activity

--- AppWithSettings/app/src/main/java/com/example/appwithsettings/MainActivity.kt
+++ AppWithSettings/app/src/main/java/com/example/appwithsettings/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        displayPreferenceFragmentCompat(SettingsFragment())
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
