553476050ab603d743181b2fa26dbcf2179dbb1d
pamunoz <pfariasmunoz@gmail.com>
2019-04-10 14:26:10 -0400

    Add the recyclerview to MainActivity

--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/MainActivity.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/MainActivity.kt
@@ -5,8 +5,10 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        rv_words.apply {
+            adapter = WordListAdapter(this@MainActivity)
+            layoutManager = LinearLayoutManager(this@MainActivity)
         }
     }
 
