97c37d9731b9e2046f9284ba7ff0f5cee68fdd5d
pamunoz <pfariasmunoz@gmail.com>
2019-04-10 08:30:42 -0400

    Remove the null object and move the asynctask to a companion object

--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/WordRepository.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/WordRepository.kt
@@ -8,24 +8,26 @@ import com.pfariasmunoz.roomwordssample.db.WordDao
 import com.pfariasmunoz.roomwordssample.db.WordRoomDatabase
 
 class WordRepository(application: Application) {
-    private var mWordDao: WordDao? = null
-    private var mAllWords: LiveData<List<Word>>? = null
+    private var mWordDao: WordDao
+    private var mAllWords: LiveData<List<Word>>
 
     init {
         val db = WordRoomDatabase.getDatabase(application)
-        mWordDao = db?.wordDao()
-        mAllWords = mWordDao?.getAllWords()
+        mWordDao = db?.wordDao()!!
+        mAllWords = mWordDao.getAllWords()
     }
 
     val allWords: LiveData<List<Word>>? = mAllWords
 
     fun insert(word: Word) {
-
+        InsertAsyncTask(mWordDao).execute(word)
     }
 
-    inner class InsertAsyncTask(private val dao: WordDao): AsyncTask<Word, Unit, Unit>() {
-        override fun doInBackground(vararg params: Word) {
-            dao.intert(params[0])
+    companion object {
+        private class InsertAsyncTask(private val dao: WordDao): AsyncTask<Word, Unit, Unit>() {
+            override fun doInBackground(vararg params: Word) {
+                dao.intert(params[0])
+            }
         }
     }
 
