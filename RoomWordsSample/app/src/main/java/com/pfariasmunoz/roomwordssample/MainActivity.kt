23e78961e71d210f00380c564e4bc7714e1e550a
pamunoz <pfariasmunoz@gmail.com>
2019-04-10 15:38:15 -0400

    Insert a word with the intent

--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/MainActivity.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.pfariasmunoz.roomwordssample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.pfariasmunoz.roomwordssample.db.Word
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val NEW_WORD_ACTIVITY_REQUEST_CODE = 1
+    }
+
+    private var mViewModel: WordViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +32,12 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-
         val wordAdapter = WordListAdapter(this@MainActivity)
-
-        ViewModelProviders.of(this ).get(WordViewModel::class.java).allwords.observe(this, Observer {
-            wordAdapter.words = it
-        })
-
+        mViewModel = ViewModelProviders.of(this ).get(WordViewModel::class.java).apply {
+            allwords.observe(this@MainActivity, Observer {
+                wordAdapter.words = it
+            })
+        }
         rv_words.apply {
             adapter = wordAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -48,6 +57,18 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if ((requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE) and (resultCode == Activity.RESULT_OK)) {
+            if (data != null) {
+                mViewModel?.insert(Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY)))
+            } else {
+                Toast.makeText(applicationContext, R.string.empty_not_saved, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/NewWordActivity.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/NewWordActivity.kt
@@ -2,10 +2,9 @@ package com.pfariasmunoz.roomwordssample
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import androidx.core.text.TextUtilsCompat
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_new_word.*
 
 class NewWordActivity : AppCompatActivity() {
