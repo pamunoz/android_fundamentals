52b7fd369beb3053a88b3898bb8488bb37ddc1c0
pamunoz <pfariasmunoz@gmail.com>
2019-04-10 07:15:24 -0400

    Change to return liveData

--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/db/WordDao.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/db/WordDao.kt
@@ -1,5 +1,6 @@
 package com.pfariasmunoz.roomwordssample.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -11,5 +12,5 @@ interface WordDao {
     @Query("DELETE FROM word_table")
     fun deleteAll()
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 }
