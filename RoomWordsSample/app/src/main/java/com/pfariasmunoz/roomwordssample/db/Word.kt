08fbc5ec7fbad90f6512fca466895c8e3832065d
pamunoz <pfariasmunoz@gmail.com>
2019-04-10 07:06:41 -0400

    Add the annotations for the entity

--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/db/Word.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/db/Word.kt
@@ -1,3 +1,9 @@
 package com.pfariasmunoz.roomwordssample.db
 
-data class Word(val word: String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "word_table")
+data class Word(@PrimaryKey @NotNull @ColumnInfo(name = "word") val word: String)
