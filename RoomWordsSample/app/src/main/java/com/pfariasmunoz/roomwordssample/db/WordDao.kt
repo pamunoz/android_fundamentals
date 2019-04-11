31bf1ee5acb535f60eafc6a4b31f421cff4fb508
pamunoz <pfariasmunoz@gmail.com>
2019-04-11 09:46:15 -0400

    Add method to the dao to delete a word

--- RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/db/WordDao.kt
+++ RoomWordsSample/app/src/main/java/com/pfariasmunoz/roomwordssample/db/WordDao.kt
@@ -1,10 +1,7 @@
 package com.pfariasmunoz.roomwordssample.db
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface WordDao {
@@ -16,4 +13,6 @@ interface WordDao {
     fun getAllWords(): LiveData<List<Word>>
     @Query("SELECT * FROM word_table LIMIT 1")
     fun getAnyWord(): Array<Word>
+    @Delete
+    fun deleteWord(word: Word)
 }
